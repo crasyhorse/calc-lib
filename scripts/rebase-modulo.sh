0f0603c36c14bd3d071dc008a4257ef96a2d7c7e
Florian Weidinger <crasyhorse@gmx.net>
2025-11-19 10:49:04 +0100

    E2 - fix: Fix message about what the script has done

--- scripts/rebase-modulo.sh
+++ scripts/rebase-modulo.sh
@@ -6,6 +6,6 @@ git switch feature/modulo
 git rebase main
 
 echo "-------------------------------------------------------------------------"
-echo "Soeben wurde der Branch bugfix/readme aus Versehen gelöscht. Er soll"
-echo "wieder hergestellt werden."
+echo "Soeben wurde der Branch feature/modulo mit Hilfe von git rebase main"
+echo "verschoben. Die Rebase-Operation soll rückgängig gemacht werden."
 echo "-------------------------------------------------------------------------"
